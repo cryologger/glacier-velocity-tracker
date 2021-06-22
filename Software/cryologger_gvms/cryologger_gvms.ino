56ba86ee85857efe29cd949a95dc9a62aa7a5265
Adam <adam.garbo@gmail.com>
2021-06-22 09:10:05 -0400

    Update KiCad files

--- Software/cryologger_gvms/cryologger_gvms.ino
+++ Software/cryologger_gvms/cryologger_gvms.ino
@@ -1,6 +1,6 @@
 /*
     Title:    Cryologger - Glacier Velocity Measurement System (GVMS) v2.0.2
-    Date:     April 27, 2021
+    Date:     June 22, 2021
     Author:   Adam Garbo
 
     Components:
@@ -66,22 +66,22 @@ SFE_UBLOX_GNSS    gnss;       // I2C address: 0x42
 // ----------------------------------------------------------------------------
 
 // Logging mode 
-byte          loggingMode           = 2;    // 1 = daily, 2 = rolling
+byte          loggingMode           = 1;    // 1 = daily, 2 = rolling
 
 // Daily alarm
 byte          loggingStartTime      = 16;   // Logging start hour (UTC)
 byte          loggingStopTime       = 19;   // Logging end hour (UTC)
 
 // Rolling alarm
-byte          loggingAlarmMinutes   = 0;    // Rolling minutes alarm
-byte          loggingAlarmHours     = 3;    // Rolling hours alarm
-byte          sleepAlarmMinutes     = 0;    // Rolling minutes alarm
-byte          sleepAlarmHours       = 1;    // Rolling hours alarm
+byte          loggingAlarmMinutes   = 5;    // Rolling minutes alarm
+byte          loggingAlarmHours     = 0;    // Rolling hours alarm
+byte          sleepAlarmMinutes     = 5;    // Rolling minutes alarm
+byte          sleepAlarmHours       = 0;    // Rolling hours alarm
 
 // Alarm modes
-byte          loggingAlarmMode      = 4;    // Logging alarm mode
-byte          sleepAlarmMode        = 4;    // Sleep alarm mode
-byte          initialAlarmMode      = 4;    // Initial alarm mode
+byte          loggingAlarmMode      = 5;    // Logging alarm mode
+byte          sleepAlarmMode        = 5;    // Sleep alarm mode
+byte          initialAlarmMode      = 5;    // Initial alarm mode
 
 // ----------------------------------------------------------------------------
 // Global variable declarations
